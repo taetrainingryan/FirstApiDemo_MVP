64d77645938b72ed2e506026eebd522e7b4fde7e
taetrainingryan <33837187+taetrainingryan@users.noreply.github.com>
2017-11-24 13:07:36 +0000

    Creation of the presenter is now done.

--- app/src/main/java/com/example/ryan/firstapidemo/MainActivity.java
+++ app/src/main/java/com/example/ryan/firstapidemo/MainActivity.java
@@ -20,15 +20,15 @@ import android.support.v7.widget.RecyclerView;
 public class MainActivity extends AppCompatActivity {
 
 //    private RequestInterface requestInterface;
-//    private RecyclerView recyclerView;
-//    private List<CakeModel> cakeModels;
+      private RecyclerView recyclerView;
+      private List<CakeModel> cakeModels;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        recyclerView = (RecyclerView) findViewById(R.id.rvCakes) ;
+        recyclerView = (RecyclerView) findViewById(R.id.rvCakes) ;
 //
 //        requestInterface = ServerConnection.getServerConnection();
 //        requestInterface.getCakesList()
--- app/src/main/java/com/example/ryan/firstapidemo/views/cakelist/CakeListPresenter.java
+++ app/src/main/java/com/example/ryan/firstapidemo/views/cakelist/CakeListPresenter.java
@@ -1,10 +1,15 @@
 package com.example.ryan.firstapidemo.views.cakelist;
 
 import com.example.ryan.firstapidemo.data.IDataManager;
+import com.example.ryan.firstapidemo.model.CakeModel;
 import com.example.ryan.firstapidemo.views.ui.base.BasePresenter;
 import com.example.ryan.firstapidemo.views.ui.utils.rx.SchedulerProvider;
 
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
 
 /**
  * Created by Ryan on 24/11/2017.
@@ -21,6 +26,28 @@ public class CakeListPresenter <V extends ICakeListMvpView>
 
     @Override
     public void onCallCakeList() {
+
+        getCompositeDisposable().add(
+                getDataManager().getFromApi_CakeList()
+                .observeOn(getSchedulerProvider().ui())
+                .subscribeOn(getSchedulerProvider().io())
+                .subscribe(new Consumer<List<CakeModel>>() {
+                    @Override
+                    public void accept(List<CakeModel> cakeModels) throws Exception {
+
+                        getMvpView().onFetchDataSuccess(cakeModels);
+
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) throws Exception {
+
+                        getMvpView().onError(throwable.getMessage());
+
+                            }
+
+                     })
+            );
 
     }
 }
